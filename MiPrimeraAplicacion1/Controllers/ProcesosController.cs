58c24be6294a0a5752a3528d984b3276d1f8f745
edi <emendoza19@hotmail.com>
2019-12-16 23:26:39 -0500

    version 1

---
+++ MiPrimeraAplicacion1/Controllers/ProcesosController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace MiPrimeraAplicacion1.Controllers
+{
+    public string Listado()
+    {
+
+        return "Este es mi Listado de Procesos";
+    }
+}
+
+
---
+++ MiPrimeraAplicacion1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MiPrimeraAplicacion1.MvcApplication" Language="C#" %>
